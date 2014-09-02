456de9b7bdd19712b5584c6666fa13d16952792e
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-09-02 14:58:24 +0200

    add basic profiles
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

---
+++ manifests/init.pp
@@ -0,0 +1,20 @@
+# File::      init.pp
+# Author::    S. Varrette, H. Cartiaux, V. Plugaru <hpc-sysadmins@uni.lu>
+# Copyright:: Copyright (c) 2014 S. Varrette, H. Cartiaux, V. Plugaru
+# License::   Apache-2.0
+#
+# ------------------------------------------------------------------------------
+# Public Puppet profiles, to be used for instance in the [vagrant-vms]()
+# repository designed to automate the generation of Vagrant boxes out of packer,
+# librarian-puppet and/or r10k
+#
+# Resources: 
+# * https://github.com/rnelson0/rnelson0-profile
+
+
+class profile {
+    
+
+
+    
+}
